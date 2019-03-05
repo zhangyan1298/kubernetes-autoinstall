bc91884cf3c054e9dd6318a493388dc757d67788
zhangyan1298 <38239794+zhangyan1298@users.noreply.github.com>
2019-03-05 17:04:21 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -73,10 +73,10 @@ tar xzvf $soft_location/flannel-${flannel_ver}-linux-amd64.tar.gz -C $soft_locat
 tar xzvf $soft_location/kubernetes-server-linux-amd64.tar.gz -C $soft_location
 tar xzvf $soft_location/kubernetes-node-linux-amd64.tar.gz -C $soft_location
 
-cp $soft_location/etcd-${etcd_ver}-linux-amd64/{etcd,etcdctl} /usr/local/sbin/
+cp $soft_location/etcd-${etcd_ver}-linux-amd64/{etcd,etcdctl} /usr/local/bin/
 cp $soft_location/flanneld /usr/local/bin/
 cp $soft_location/mk-docker-opts.sh /usr/local/bin
-cp $soft_location/kubernetes/node/bin/* /usr/local/sbin/
+cp $soft_location/kubernetes/node/bin/* /usr/local/bin/
 cp $soft_location/kubernetes/server/bin/{kubelet,kubectl,kube-apiserver,kube-controller-manager,kube-scheduler} /usr/local/sbin
 
 
